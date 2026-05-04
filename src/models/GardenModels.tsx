e33ff31e7b5fac4b04544448aeddb321738439e3
Florian Dyck <floriandyck17@gmail.com>
2026-05-04 19:03:48 +0200

    neue Sprites

--- src/models/GardenModels.tsx
+++ src/models/GardenModels.tsx
@@ -63,11 +63,14 @@ export function GardenModels() {
       {/*<Grass position={[-4, 0, -3]} /> */}
 
       {/* 2D Sprite auf dem Boden */}
+      {/* old
       <GroundSprite position={[0, 0, 0]} url="/sprites/origin-tree-transparent.png" scale={30} />
       <GroundSprite position={[0, 0, 0]} url="/sprites/origin-tree-solo.png" scale={30} renderOrder={3} depthTest={false} receiveShadow={false} />
       <GroundSprite position={[-52, 0, -52]} url="/sprites/music-production-area-transparent.png" scale={20} />
       <GroundSprite position={[-20, 0, 0]} url="/sprites/path-isles.png" scale={20} />
       <GroundSprite position={[30, 0, -30]} url="/sprites/game-dev-area.png" scale={30} />
+      */}
+      <GroundSprite position={[0, 0, 0]} url="/sprites/whole.png" scale={50} />
     </>
   )
 }
